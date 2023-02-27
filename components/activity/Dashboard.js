5e38f6dd568e21f364d9a04612ba4eb8d5446ace
Xing Ern <lxingern@gmail.com>
2023-02-27 15:27:09 +0800

    retrieve todays data and render dynamically in dashboard

--- components/activity/Dashboard.js
+++ components/activity/Dashboard.js
@@ -2,12 +2,21 @@ import Donut from "./Donut"
 import ActivityList from "./ActivityList"
 
 const Dashboard = () => {
+    let todaysData = null
+    
+    const getTodaysData = async () => {
+        const response = await fetch('/api/activity/get-todays-data')
+        todaysData = await response.json()
+    } 
+
+    getTodaysData()
+
     return (
         <div className="flex flex-col items-center">
             <p className="text-2xl mb-5 font-bold">24 Feb 2023</p>
             <div className="flex mb-8">
-                <Donut count="6000" unit="steps" />
-                <Donut count="40" unit="mins" />
+                <Donut count={todaysData ? todaysData.data.steps : 0} unit="steps" />
+                <Donut count={todaysData ? todaysData.data.activeMinutes : 0} unit="mins" />
             </div>
             <div className="w-2/3 text-lg">
                 <ActivityList />

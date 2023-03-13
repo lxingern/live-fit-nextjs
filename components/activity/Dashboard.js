9d1a174cb58c204b12ca984269f275443aa7f054
Xing Ern <lxingern@gmail.com>
2023-03-13 15:26:57 +0800

    display modal on clicking step count in dashboard

--- components/activity/Dashboard.js
+++ components/activity/Dashboard.js
@@ -1,15 +1,30 @@
+import { useState } from "react"
 import dayjs from "dayjs"
 import Donut from "./Donut"
 import ActivityList from "./ActivityList"
+import Modal from "../UI/Modal"
 
 const Dashboard = (props) => {
     const todaysData = props.data
+    const [showEditSteps, setShowEditSteps] = useState(false)
+
+    const showEditStepsHandler = () => {
+        setShowEditSteps(true)
+        console.log('clicked')
+    }
 
     return (
         <div className="flex flex-col items-center">
             <p className="text-2xl mb-5 font-bold">{dayjs(new Date()).format('D MMM YYYY')}</p>
             <div className="flex mb-8">
-                <Donut count={todaysData ? todaysData.data.steps : 0} unit="steps" />
+                <Donut count={todaysData ? todaysData.data.steps : 0} unit="steps" clickHandler={showEditStepsHandler} />
+                {showEditSteps && <Modal> 
+                    <h2>Edit Steps</h2>
+                    <form>
+                        <input type="number" />
+                        <button>Edit</button>
+                    </form>
+                </Modal>}
                 <Donut count={todaysData ? todaysData.data.activeMinutes : 0} unit="mins" />
             </div>
             <div className="w-2/3 text-lg">
--- components/activity/Donut.js
+++ components/activity/Donut.js
@@ -1,6 +1,6 @@
 const Donut = (props) => {
     return (
-        <div className="flex flex-col items-center text-lg w-36 mx-10">
+        <div className="flex flex-col items-center text-lg w-36 mx-10 cursor-pointer" onClick={props.clickHandler}>
             <div className="donut w-28 h-28 flex justify-center items-center rounded-full">
                 <div className="bg-white rounded-full flex flex-col justify-center items-center w-20 h-20">
                     <p>{props.count}</p>

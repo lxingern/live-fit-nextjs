00f3e6e299f0cef30ab0a5e4b2b1ccad15fb7306
Xing Ern <lxingern@gmail.com>
2023-03-13 15:26:34 +0800

    fix bugs in Modal component

--- components/UI/Modal.js
+++ components/UI/Modal.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react"
+import ReactDOM from "react-dom"
+
 const Backdrop = () => {
   return <div className="backdrop" />
 }
@@ -10,13 +13,23 @@ const ModalOverlay = (props) => {
   )
 }
 
-const Modal = () => {
-  return (
-    <>
-      <Backdrop />
-      <ModalOverlay />
-    </>
-  )
+const Modal = (props) => {
+  const [isBrowser, setIsBrowser] = useState(false);
+  
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  if (isBrowser) {
+    return (
+      <>
+        {ReactDOM.createPortal(<Backdrop />, document.getElementById('overlays'))}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, document.getElementById('overlays'))}
+      </>
+    )
+  } else {
+    return null
+  }
 }
 
 export default Modal
